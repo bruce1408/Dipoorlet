efb26c8806beb264a6cc7d944744e8df402a3e70
bruce1408 <summer56567@163.com>
2024-10-30 01:48:02 +0800

    feat(example): 删除mock数据生成和运行脚本

--- example/demo_1_classify/run_demo_1.sh
+++ example/demo_1_classify/run_demo_1.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python -m torch.distributed.launch --use_env \
+torchrun \
 -m dipoorlet \
 -M /mnt/share_disk/bruce_trie/misc_data_products/onnx_models/resnet34_model.onnx \
--I /mnt/share_disk/bruce_trie/Quantizer-Tools/Dipoorlet/example/demo_1/ \
+-I /mnt/share_disk/bruce_trie/Quantizer-Tools/Dipoorlet/example/demo_1_classify/ \
 -N 2 \
 -A minmax \
 -D trt
