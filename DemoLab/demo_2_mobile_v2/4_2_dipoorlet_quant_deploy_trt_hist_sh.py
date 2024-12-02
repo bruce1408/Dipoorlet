7ef67bf6ddcad4e4bf38d3d805cdec8f8be25487
bruce1408 <summer56567@163.com>
2024-12-02 23:22:30 +0800

    feat: 添加subprocess模块并优化CUDA环境变量设置

--- DemoLab/demo_2_mobile_v2/4_2_dipoorlet_quant_deploy_trt_hist_sh.py
+++ DemoLab/demo_2_mobile_v2/4_2_dipoorlet_quant_deploy_trt_hist_sh.py
@@ -1,7 +1,7 @@
 import os, sys
+import subprocess
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import demo_utils.quant_config as config
-import subprocess
 
 # 构建 CUDA 环境变量
 os.environ["CUDA_VISIBLE_DEVICES"] = config.cuda_ids
