95d4706a8b429aca7d6f7ec3003238c32814b4f9
bruce1408 <summer56567@163.com>
2025-09-18 16:14:43 +0800

    refactor: 在allocate_buffers函数中添加注释以提高代码可读性，明确主机和设备内存的分配过程

--- DemoLab/demo_2_mobile_v2/6_test_quant_acc.py
+++ DemoLab/demo_2_mobile_v2/6_test_quant_acc.py
@@ -43,11 +43,14 @@ def allocate_buffers(engine):
     for binding in engine:
         size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
         dtype = trt.nptype(engine.get_binding_dtype(binding))
+        
         # Allocate host and device buffers
         host_mem = cuda.pagelocked_empty(size, dtype)
         device_mem = cuda.mem_alloc(host_mem.nbytes)
+        
         # Append the device buffer to device bindings.
         bindings.append(int(device_mem))
+        
         # Append to the appropriate list.
         if engine.binding_is_input(binding):
             inputs.append(HostDeviceMem(host_mem, device_mem))
