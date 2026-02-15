0c039d4c8163dba5c04f145fd92b24c43996ff54
bruce1408 <summer56567@163.com>
2026-02-15 19:31:38 +0800

    更新: 优化CUDA环境变量设置，调整代码格式以提升可读性，并在主函数中添加TensorRT准确度计算功能，增强脚本的功能性和可维护性

--- DemoLab/3_quant_resnet18/3_1_resnet18_dipoorlet_trt/4_2_dipoorlet_quant_deploy_trt_hist_sh.py
+++ DemoLab/3_quant_resnet18/3_1_resnet18_dipoorlet_trt/4_2_dipoorlet_quant_deploy_trt_hist_sh.py
@@ -1,14 +1,19 @@
 import os, sys
 import subprocess
+
 # import dipoorlet_utils.quant_config as config
 from common.configs import get_cfg_defaults
 from calculate_trt_engine_acc import calculate_tensorrt_acc
+
 cfg = get_cfg_defaults()
 
 # 构建 CUDA 环境变量
 os.environ["CUDA_VISIBLE_DEVICES"] = cfg.SYSTEM.CUDA_IDS
-os.environ["OMP_NUM_THREADS"] = cfg.DIPOORLET.OMP_NUM_THREADS  # 设置OpenMP线程数，可以根据CPU核心数调整
+os.environ["OMP_NUM_THREADS"] = (
+    cfg.DIPOORLET.OMP_NUM_THREADS
+)  # 设置OpenMP线程数，可以根据CPU核心数调整
 cuda_num = len(cfg.SYSTEM.CUDA_IDS.split(","))
+
 
 def main():
     
@@ -16,7 +21,7 @@ def main():
     os.makedirs(log_dir, exist_ok=True)
     
     onnx_path = f"{cfg.SYSTEM.MODELS_DIR}/resnet18.onnx"
-    calibration_data = f"{cfg.DIPOORLET.dipoorlet_calib_data_dir}/resnet18_calib/" 
+    calibration_data = f"{cfg.DIPOORLET.dipoorlet_calib_data_dir}/resnet18_calib/"
 
     # 构建 torchrun 命令
     command = [
@@ -35,6 +40,10 @@ def main():
     
     # 执行命令
     subprocess.run(command, check=True)
+    calculate_tensorrt_acc(
+        engine_file=f"{log_dir}/resnet18_model_dipoorlet_hist_int8.engine"
+    )
+
 
 if __name__ == "__main__":
     main()
